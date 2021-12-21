48b6671f8d803a60fc978d22e2049dd10224ff06
proXDhiya <prox.dhiya@gmail.com>
2021-12-21 11:13:30 +0100

    add first operators - + * /

--- compile.sh
+++ compile.sh
@@ -1,2 +1,15 @@
 # compile code.l from flex to c
 flex code.l
+
+
+# compile code.y from bison to c
+bison -dy code.y
+
+# compile code.tab.c code.tab.h lex.yy.c to binary file
+gcc lex.yy.c y.tab.c -o code.out
+
+# remove code.tab.c code.tab.h lex.yy.c
+rm y.tab.c y.tab.h lex.yy.c
+
+# run code.out
+./code.out
